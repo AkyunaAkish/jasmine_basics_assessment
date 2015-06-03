96c93fe5445d1ddd62985f0fd882ba1a4c40a4c6
AkyunaAkish <akyunaakish@gmail.com>
2015-06-03 17:14:40 -0600

    Did challenges

---
+++ grader.js
@@ -0,0 +1,73 @@
+module.exports = {
+  letterGrader: function(a){
+  if(a >= 95 && a <= 100){
+    return 'A+';
+  }
+   if(a >= 85 && a < 90){
+    return 'B+';
+  }
+   if(a >= 75 && a < 80){
+    return 'C+';
+  }
+   if(a >= 65 && a < 70){
+    return 'D+';
+  }
+   if( a < 60){
+    return 'F';
+  }
+   if(a >= 90 && a < 95){
+    return 'A-';
+  }
+   if(a >= 80 && a < 85){
+    return 'B-';
+  }
+   if(a >= 70 && a < 75){
+    return 'C-';
+  }
+   if(a >= 60 && a < 65){
+    return 'D-';
+  }
+},
+  averageScore: function(arr){
+    var i = 0;
+    var sum = 0;
+    while(i < arr.length){
+      sum = sum + arr[i];
+      i++;
+    }
+    var average = sum / arr.length;
+    return average;
+  },
+  medianScore: function(values) {
+    values.sort( function(a,b) {return a - b;} );
+
+    var half = Math.floor(values.length/2);
+
+    if(values.length % 2){
+      return values[half];
+    }
+    else{
+      return (values[half-1] + values[half]) / 2.0;
+    }
+  },
+  modeScore: function(array) {
+      if(array.length === 0)
+      	return null;
+      var modeMap = {};
+      var maxEl = array[0], maxCount = 1;
+      for(var i = 0; i < array.length; i++)
+      {
+      	var el = array[i];
+      	if(modeMap[el] == null)
+      		modeMap[el] = 1;
+      	else
+      		modeMap[el]++;
+      	if(modeMap[el] > maxCount)
+      	{
+      		maxEl = el;
+      		maxCount = modeMap[el];
+      	}
+      }
+      return maxEl;
+  }
+};
---
+++ spec/spec.js
@@ -0,0 +1,25 @@
+var myCode = require('./../grader.js');
+
+describe('#letterGrader()', function() {
+  it('Returns grade equivalent of a number', function(){
+    expect(myCode.letterGrader(96)).toEqual("A+");
+
+  });
+});
+describe('#averageScore()', function() {
+  it('Returns average score of an array', function(){
+    expect(myCode.averageScore([90, 95, 87, 60])).toEqual(83);
+
+  });
+});
+
+describe('#medianScore()', function() {
+  it('Returns the median of an array', function(){
+    expect(myCode.medianScore([52,80,80,86,94])).toEqual(80);
+   });
+  });
+  describe('#modeScore()', function() {
+    it('Returns the mode of an array', function(){
+      expect(myCode.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
+     });
+    });
